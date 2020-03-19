f0e7d4605e1961f661f6bf4add0eed81f552fc11
Vindo Kountur <vindokountur@gmail.com>
2020-03-19 16:08:09 +0800

    add: main route

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const express = require('express');
-
+const axios = require('axios');
 const app = express();
 
-const provinsi = require('./router/provinsi')
+const provinsi = require('./router/provinsi');
+
+let fetchData = async (url) => {
+    const res = await axios.get(url);
+    const data = res.data;
+    return data;
+}
+// let tesFu  = async () => {
+//   let data = await fetchData('https://covid19.mathdro.id/api');
+//   // WORLD
+//   world = {
+//     confirmed : data.confirmed.value
+//   }
+// }
+// let world;
+
+// tesFu();
 
 app.use("/api/provinsi", provinsi);
+
+app.get("/", (req, res) => {
+  res.status(200).json(
+    {
+      success: true,
+      routes : {
+        provinsi : "/api/provinsi"
+      }
+    }
+  )
+})
 
 const PORT = process.env.PORT || 5000;
 
